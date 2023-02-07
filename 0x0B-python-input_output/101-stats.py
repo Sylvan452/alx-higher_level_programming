1d928c5de7a2cdfafcf3722c6a762c3a07c9b8f1
sylvan452 <sylvan452@gmail.com>
2023-02-07 19:45:32 +0100

    stats

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 """
 reads stdin line by line and computes metrics
 """
+
 import sys
 
 file_size = 0
@@ -11,30 +13,28 @@ i = 0
 try:
     for line in sys.stdin:
         tokens = line.split()
-        if len(tokens) >= 2:
-            a = i
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
+        if len(tokens) >= 5:
+            status = tokens[-2]
+            if status in status_tally:
+                status_tally[status] += 1
             try:
                 file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
-            except FileNotFoundError:
-                if a == i:
-                    continue
+            except ValueError:
+                continue
+        i += 1
         if i % 10 == 0:
-            print("File size: {:d}".format(file_size))
+            print("Total file size: {:d}".format(file_size))
             for key, value in sorted(status_tally.items()):
                 if value:
                     print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(file_size))
+    print("Total file size: {:d}".format(file_size))
     for key, value in sorted(status_tally.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
 
 except KeyboardInterrupt:
-    print("File size: {:d}".format(file_size))
+    print("Total file size: {:d}".format(file_size))
     for key, value in sorted(status_tally.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
+
