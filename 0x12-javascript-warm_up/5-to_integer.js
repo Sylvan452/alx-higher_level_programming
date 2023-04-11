bf5a98cb271b09c5d6bce460e810ec42f3c1bf76
“sylvan452” <sylvan452@gmail.com>
2023-04-11 08:38:04 -0700

    typo corrected

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-cost process = require('process');
+const process = require('process');
 const args = process.argv;
 
-if (isNAN(args[2])){
-	console.log('Not a number');
-}else {
-	console.log('My number',args[2]);
+if (isNaN(args[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', args[2]);
 }
