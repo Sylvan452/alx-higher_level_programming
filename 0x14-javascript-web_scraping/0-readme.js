a641862bda5482d3204e05f723cf2244a271d7a3
sylvan452 <sylvan452@gmail.com>
2023-05-23 12:10:06 +0000

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  if (error) {
-    console.error(`Error occurred while reading the file: ${error}`);
-  } else {
-    console.log(content);
-  }
+  console.log(error || content);
 });
-
